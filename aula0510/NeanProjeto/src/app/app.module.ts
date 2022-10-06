b23f1e173081670d06943df41d07d177a6d6ebce
GrupoSequencial <33881813+ericaolopess@users.noreply.github.com>
2022-10-05 21:15:58 -0300

    05/10

--- aula0510/NeanProjeto/src/app/app.module.ts
+++ aula0510/NeanProjeto/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { AppComponent } from './app.component';
 import { ClienteInserirComponent } from './clientes/cliente-inserir/cliente-inserir.component';
 import { CabecalhoComponent } from './cabecalho/cabecalho.component'
-import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 
 @NgModule({
